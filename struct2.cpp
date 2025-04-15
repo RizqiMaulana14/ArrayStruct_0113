b0ad99d3cb98be46e92535c76d97f92ee12e8a53
RizqiMaulana14 <maulanafitrah14@gmail.com>
2025-04-15 20:35:51 +0700

    Struct Kedua

---
+++ struct2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat{
+    string desa;
+    string kota;
+};
+
+struct mahasiswa{
+    string nim;
+    string nama;
+    detailAlamat alamat;
+};
+
+int main(){
+    //membuat object struct mahasiswa
+    mahasiswa mhs;
+
+    //mengakses struct mahasiswa
+    mhs.nim = "2024014113";
+    mhs.nama = "Rizqi";
+    mhs.alamat.desa = "Tamantirto";
+    mhs.alamat.kota = "DIY";
+
+    cout << "Masukkan NIM = ";
+    cin >> mhs.nim;
+    cout << "Masukkan Nama = ";
+    cin >> mhs.nama;
+    cout << "Masukkan Alamat Desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "Masukkan Alamat Kota = ";
+    cin >> mhs.alamat.kota;
+
+    cout << "NIM = " << mhs.nim << endl;
+    cout << "NAMA = " << mhs.nama << endl;
+    cout << "ALAMAT DESA = " << mhs.alamat.desa << endl;
+    cout << "ALAMAT KOTA = " << mhs.alamat.kota << endl;
+}
